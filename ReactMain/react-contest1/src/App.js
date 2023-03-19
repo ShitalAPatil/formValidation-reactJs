0e8810da3ca03183dc762a833c0d54a08066afa5
ShitalAPatil <shitalpatilb29@gmail.com>
2023-03-19 14:10:49 +0530

    initial commit

---
+++ ReactMain/react-contest1/src/App.js
@@ -0,0 +1,15 @@
+
+
+import SignUp from './components/SignUp'
+import './App.css';
+
+const App = () => {
+    return (
+        <div>
+            <SignUp />
+        </div>
+    )
+}
+
+export default App
+
